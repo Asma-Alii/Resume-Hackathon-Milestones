8f0124659ad496e7fc6b42d30f69285648234356
Asma <160001159+Asma-Alii@users.noreply.github.com>
2024-11-27 23:19:10 +0500

    Add files via upload

---
+++ milestone_1/script.ts
@@ -0,0 +1,13 @@
+const skills = document.querySelector('ul') as HTMLElement;
+const button = document.getElementById('toggle-button') as HTMLButtonElement;
+
+
+button.addEventListener('click', () => {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+          button.innerHTML = '<i class="fa-solid fa-caret-up"></i>'
+    } else {
+        skills.style.display = 'none';
+        button.innerHTML ='<i class="fa-solid fa-caret-down" id="down"></i>'
+    }
+});
